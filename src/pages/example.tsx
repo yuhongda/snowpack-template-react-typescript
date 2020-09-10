9c8ec2e17e83f578d53baa1f14bab71bec08b3ae
yuhongda <silverage.y@gmail.com>
2020-09-10 16:10:47 +0800

    echarts

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -7,9 +7,15 @@ import './example.css';
 import styles from './test.module.scss';
 import stylesLess from './testLess.module.less';
 import logo from '../logo.svg';
+import ReactEcharts from 'echarts-for-react';
 
 const StyledLink = styled.a`
   color: #01439c;
+`;
+
+const ChartContainer = styled.div`
+  width: 600px;
+  height: 400px;
 `;
 
 const Example: React.FC<IProps> = (props) => {
@@ -30,12 +36,34 @@ const Example: React.FC<IProps> = (props) => {
     <div className="example">
       <div className="example-inner">
         <img src={logo} className="example-logo" alt="logo" />
-        <StyledLink className="example-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <StyledLink className="example-link" href="https://www.snowpack.dev" target="_blank" rel="noopener noreferrer">
           Hello Snowpack!!
         </StyledLink>
         <div className={stylesLess.blue}>{localStore.getText}</div>
         <div className={styles.red}>{localStore.getText}</div>
         <Button type="primary">antd test</Button>
+        <ChartContainer>
+          <ReactEcharts
+            option={{
+              xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              },
+              yAxis: {
+                type: 'value',
+              },
+              series: [
+                {
+                  data: [820, 932, 901, 934, 1290, 1330, 1320],
+                  type: 'line',
+                },
+              ],
+            }}
+            notMerge={true}
+            opts={{ renderer: 'svg' }}
+            style={{ height: '100%', width: '100%' }}
+          />
+        </ChartContainer>
       </div>
     </div>
   ));
